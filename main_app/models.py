8bd47c670ba922412485290838206253640ea32c
James Shin <jddshin@gmail.com>
2022-02-11 12:24:25 -0500

    Add User Model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from pyuploadcare.dj.forms import ImageField
 
 # Create your models here.
+class wfUser(models.Model):
+    # username, password, email, firstname, lastname already used by Django
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    date_joined = models.DateField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    profile_photo = ImageField(blank=True, manual_crop="")
