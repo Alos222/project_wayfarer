c261eb44c559a1e5e741317b0dac2af8dab9c3c5
Alos22 <aniriver689@gmail.com>
2022-02-11 13:15:53 -0500

    progress

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
 
 class Location(models.Model):
     city = models.CharField(max_length=200)
-    img = models.CharField()
+    country = models.CharField(max_length=100)
+    img = ImageField(blank=True, manual_crop="")
