4ffb2690e16d387461a5dcc73418d570ca1061e1
Alos22 <aniriver689@gmail.com>
2022-02-11 14:03:19 -0500

    model complete for location

--- main_app/models.py
+++ main_app/models.py
@@ -7,3 +7,5 @@ class Location(models.Model):
     city = models.CharField(max_length=200)
     country = models.CharField(max_length=100)
     img = ImageField(blank=True, manual_crop="")
+    POI = models.ForeignKey(Point, on_delete=models.CASCADE, related_name="point of interest")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post")
