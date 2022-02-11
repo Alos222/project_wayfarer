699cf9fcc65f3b0c976b6194a87861acc1bf323a
James Shin <jddshin@gmail.com>
2022-02-11 16:05:41 -0500

    Add skeleton code for paths and views

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from main_app.models import WayfarerUser, Post, Location
+
 # Register your models here.
+admin.site.register(WayfarerUser)
+admin.site.register(Post)
+admin.site.register(Location)
--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,15 @@ from django.views.generic import TemplateView
 # Create your views here.
 class Home(TemplateView):
     template_name = 'home.html'
+    
+class About(TemplateView):
+    template_name = 'about.html'
+    
+class Discover(TemplateView):
+    template_name = 'discover.html'
+    
+class Profile(TemplateView):
+    template_name = 'profile.html'
+    
+class Post(TemplateView):
+    template_name = 'single_post.html'
