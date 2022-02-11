5b277ec000d32be016d52b503a182a141c6417b4
Floreti Designs <workyg@protonmail.com>
2022-02-11 13:53:35 -0500

    Updated Models.py file with Post model Updated settings.py file

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from pyuploadcare.dj.forms import ImageField 
 
-# Create your models here.
+# Create your models here.    
+class Post(models.Model):
+        
+    title = models.CharField(max_length=200)
+    user = models.CharField(max_length=300)
+    content = models.CharField(max_length=2000)
+    content_img = models.ImageField()
+    content_imgtxt = models.CharField(max_length=500)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="posts")
+
+    def __str__(self):
+        return self.title
+
+    
+   
