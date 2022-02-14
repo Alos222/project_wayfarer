7979d944c8ae80ae9af68d51dda98dd1c7726184
Alos22 <aniriver689@gmail.com>
2022-02-14 16:01:23 -0500

    script/style added

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, View
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from .models import Profile
+from .models import Profile, Location, Post
 from django.contrib.auth.models import User
+# Authorization
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 # Create your views here.
 class Home(TemplateView):
@@ -12,9 +15,19 @@ class Home(TemplateView):
 class About(TemplateView):
     template_name = 'about.html'
     
+@method_decorator(login_required, name='dispatch')
 class Discover(TemplateView):
     template_name = 'discover.html'
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        city = self.request.GET.get('city')
+        if city != None:
+            context['locations'] = Location.objects.filter(name__icontains=city)
+        else: 
+            context['locations'] = Location.objects.all()
+        return context
+
 class Profile(TemplateView):
     template_name = 'profile.html'
     
