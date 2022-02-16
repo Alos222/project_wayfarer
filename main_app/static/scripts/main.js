f4005542c08a3e53b67e057da81070540162dfdb
Alos22 <aniriver689@gmail.com>
2022-02-15 21:12:09 -0500

    list is now scrollable, and clicking brings up img and description

--- main_app/static/scripts/main.js
+++ main_app/static/scripts/main.js
@@ -1,11 +1,20 @@
 // Variables from discover page 
-$list = $('.city_list')
+$list = $('.image_city')
 $discovercard = $('#discovercard')
+$contentimg = $('#contentimg')
+
 
 
 // This function handles clicks for the list of locations and populates the div block with location data
 $list.click(function(){
-    let description = $(this).find('#city_description').html()
+    let img = $(this).find('#discover_img').clone()
+    console.log(img)
+    let description = $(this).find('#city_description').clone()
+    console.log(description)
     $discovercard.empty()
-    $discovercard.append(description)
+    $discovercard.append(img[0])
+    $discovercard.append(description[0])
+    $discovercard.children('p').toggleClass('d-none')
+  
+  
 })
