c57f6c50a1114b8acd02703a9ab4d7e880481d53
Alos22 <aniriver689@gmail.com>
2022-02-16 09:54:50 -0500

    Discover page is now functional with locations + descriptions

--- main_app/static/scripts/main.js
+++ main_app/static/scripts/main.js
@@ -7,14 +7,24 @@ $contentimg = $('#contentimg')
 
 // This function handles clicks for the list of locations and populates the div block with location data
 $list.click(function(){
+    // grabs image from list and then assigns new id
     let img = $(this).find('#discover_img').clone()
-    console.log(img)
+    $(img).attr('id', 'discovercard_img')
+    //grabs description and then assigns new id 
     let description = $(this).find('#city_description').clone()
+    $(description).attr('id', 'discovercard_description')
     console.log(description)
+    //grabs city name & country name and then assigns new id's 
+    let city = $(this).find('#discover_city').clone()
+    $(city).attr('id', 'discovercard_city')
+    let country = $(this).find('#discover_country').clone()
+    $(country).attr('id', 'discovercard_country')
+    // populate discover card
     $discovercard.empty()
     $discovercard.append(img[0])
+    $discovercard.append(city[0])
+    $discovercard.append(country[0])
     $discovercard.append(description[0])
+    // $discovercard.children('h5').toggleClass('d-none')
     $discovercard.children('p').toggleClass('d-none')
-  
-  
-})
+});
